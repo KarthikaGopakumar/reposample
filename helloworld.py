ea5cebd742ebf40345caae37dbf5392b0cf48d01
Karthika <karthikagopakumar909@gmail.com>
2025-02-13 16:05:01 +0530

    hii

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,18 @@
 print("helloworld")
+
+# commmand for checking git version
+
+#      git --version
+
+# command for initializing git in Pwd 
+#    git init 
+
+# command for listing all untracked files
+#       git status
+
+# command for adding untracked files 
+#     git add -A 
+
+
+# command for commit 
+#     git commit -m "any messages"
